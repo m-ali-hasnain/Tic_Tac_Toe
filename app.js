a309b968c631f99e45f6e8cee0cdab3f675b0ec6
Muhammad Hasnain Ali <79736044+m-ali-hasnain@users.noreply.github.com>
2021-04-13 19:08:52 +0500

    Task 2
    This is task 2 in which Style for game has been designed.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
@@ -11,12 +11,62 @@ import { Card } from 'react-native-paper';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+      {/* Row 1 */}
+      <View style={{ flexDirection: 'row' }}>
+        <View style={[styles.cell, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>0,0</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.cell, { borderTopWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>0,1</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.cell, { borderRightWidth: 0, borderTopWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>0,2</Text>
+          </Pressable>
+        </View>
+      </View>
+      {/* Row 2 */}
+      <View style={{ flexDirection: 'row' }}>
+        <View style={[styles.cell, { borderLeftWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>1,0</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.cell]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>1,1</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.cell, { borderRightWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>1,2</Text>
+          </Pressable>
+        </View>
+      </View>
+      {/* Row 3 */}
+      <View style={{ flexDirection: 'row' }}>
+        <View
+          style={[styles.cell, { borderLeftWidth: 0, borderBottomWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>2,0</Text>
+          </Pressable>
+        </View>
+        <View style={[styles.cell, { borderBottomWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>2,1</Text>
+          </Pressable>
+        </View>
+        <View
+          style={[styles.cell, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
+          <Pressable>
+            <Text style={styles.cellComponent}>2,2</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
@@ -24,15 +74,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: '#fff',
   },
-  paragraph: {
-    margin: 24,
+  cell: {
+    borderWidth: 3,
+    borderColor: 'green',
+    width: 100,
+    height: 100,
+    color: 'black',
+  },
+  signX: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 60,
+    color: 'red',
+    padding: 20,
+  },
+  signO: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 60,
+    padding: 20,
+    color: 'orange',
+  },
+  cellComponent: {
+    flex: 1,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
 });
