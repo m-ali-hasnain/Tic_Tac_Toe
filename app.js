b40c116dc184a95cc2b02a9f6363721988688f00
Muhammad Hasnain Ali <79736044+m-ali-hasnain@users.noreply.github.com>
2021-04-13 23:52:19 +0500

    Task 4
    Only scoring is left

--- app.js
+++ app.js
@@ -1,76 +1,126 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
+import {Component } from 'react';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      turn:1,
+      board:[],
+    };
+  }
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+  componentWillMount(){
+    this.init__game__();
+  }
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+  //init game 
+  init__game__(){
+    this.setState({
+      turn: 1,
+      board: [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+      ],
+    })
+  }
+  
+//Function to handle touch 
+handleClick = (row, col) => {
+    var my_grid = this.state.board;
+    my_grid[row][col] = this.state.turn == 1 ? 1 : 2;
+    var newTurn = (this.state.turn) == 1 ? 2 : 1;
+    this.setState({ board: my_grid, turn:newTurn});
+  };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* Row 1 */}
-      <View style={{ flexDirection: 'row' }}>
-        <View style={[styles.cell, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>0,0</Text>
+//function to render icon
+renderIcon = (row, col) => {
+  var value = this.state.board[row][col];
+  if(value==1){
+    return <Text style={styles.signX}>X</Text>
+  }
+  else if(value==2){
+    return <Text style={styles.signO}>O</Text>
+  }
+  else{
+    return <View/>
+  }
+
+};
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize:20,marginBottom:20}}>Turn Player: {this.state.turn}</Text>
+        <Text style={{fontSize:16,marginBottom:20, color:'red'}}>PLayer 1 Symbol: X</Text>
+        {/* Row 1 */}
+        
+        <View style={{ flexDirection: 'row' }}>
+          <Pressable
+            style={[styles.cell, { borderLeftWidth: 0, borderTopWidth: 0 }]}
+            onPress = {()=>{this.handleClick(0,0)}}
+            >
+            {this.renderIcon(0,0)}
+          </Pressable>
+
+          <Pressable style={[styles.cell, { borderTopWidth: 0 }]}
+           onPress = {()=>{this.handleClick(0,1)}}>
+          {this.renderIcon(0,1)}
+          </Pressable>
+
+          <Pressable
+            style={[styles.cell, { borderRightWidth: 0, borderTopWidth: 0 }]}onPress = {()=>{this.handleClick(0,2)}}>
+            {this.renderIcon(0,2)}
+          </Pressable>
+
+        </View>
+
+        {/* Row 2 */}
+        <View style={{ flexDirection: 'row' }}>  
+          <Pressable style={[styles.cell, { borderLeftWidth: 0 }]} onPress = {()=>{this.handleClick(1,0)}}>    
+          {this.renderIcon(1,0)}
+          </Pressable>
+          
+          <Pressable style={[styles.cell]} onPress = {()=>{this.handleClick(1,1)}}>
+            {this.renderIcon(1,1)}
+          </Pressable>
+          <Pressable style={[styles.cell, { borderRightWidth: 0 }]} onPress = {()=>{this.handleClick(1,2)}}>
+            {this.renderIcon(1,2)}
           </Pressable>
         </View>
-        <View style={[styles.cell, { borderTopWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>0,1</Text>
+
+        {/* Row 3 */}
+        
+        <View style={{ flexDirection: 'row' }}>
+          <Pressable
+            style={[styles.cell, { borderLeftWidth: 0, borderBottomWidth: 0 }]}
+            onPress = {()=>{this.handleClick(2,0)}}
+            >
+            {this.renderIcon(2,0)}
           </Pressable>
-        </View>
-        <View style={[styles.cell, { borderRightWidth: 0, borderTopWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>0,2</Text>
+
+          <Pressable style={[styles.cell, { borderBottomWidth: 0 }]}
+           onPress = {()=>{this.handleClick(2,1)}}>
+          {this.renderIcon(2,1)}
           </Pressable>
+
+          <Pressable
+            style={[styles.cell, { borderRightWidth: 0, borderBottomWidth: 0 }]}onPress = {()=>{this.handleClick(2,2)}}>
+            {this.renderIcon(2,2)}
+          </Pressable>
+
         </View>
+         <Text style={{fontSize:16,marginTop:20, color:'orange'}}>PLayer 2 Symbol: O</Text>
+       
       </View>
-      {/* Row 2 */}
-      <View style={{ flexDirection: 'row' }}>
-        <View style={[styles.cell, { borderLeftWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>1,0</Text>
-          </Pressable>
-        </View>
-        <View style={[styles.cell]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>1,1</Text>
-          </Pressable>
-        </View>
-        <View style={[styles.cell, { borderRightWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>1,2</Text>
-          </Pressable>
-        </View>
-      </View>
-      {/* Row 3 */}
-      <View style={{ flexDirection: 'row' }}>
-        <View
-          style={[styles.cell, { borderLeftWidth: 0, borderBottomWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>2,0</Text>
-          </Pressable>
-        </View>
-        <View style={[styles.cell, { borderBottomWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>2,1</Text>
-          </Pressable>
-        </View>
-        <View
-          style={[styles.cell, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
-          <Pressable>
-            <Text style={styles.cellComponent}>2,2</Text>
-          </Pressable>
-        </View>
-      </View>
-    </View>
-  );
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -100,12 +150,5 @@ const styles = StyleSheet.create({
     fontSize: 60,
     padding: 20,
     color: 'orange',
-  },
-  cellComponent: {
-    flex: 1,
-    fontSize: 18,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
   },
 });
